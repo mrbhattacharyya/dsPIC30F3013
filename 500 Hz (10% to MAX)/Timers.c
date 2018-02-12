9c037481ef308b11cb75de4d64fa6b3e86ca03f5
Barnik Bhattacharyya <35532272+mrbhattacharyya@users.noreply.github.com>
2018-02-12 22:50:00 +0530

    Update Timers.c

--- 500 Hz (10% to MAX)/Timers.c
+++ 500 Hz (10% to MAX)/Timers.c
@@ -17,33 +17,33 @@ extern float Duty;
 
 //..........................Timer1_Init FUNCTION................
 
-void Timer1_Init(void)
+void Timer1_Init(void)		//Generates Interrupt of about 100th of 0.5 second (Timer used to control every percent of the Duty Cycle)
 {
-        T1CON = 0x0000;        
+        T1CON = 0x0000;		//Timer Prescale option of 1:1        
                                
         PR1 = 0x01F4;  
         IFS0bits.T1IF = 0;      
-        IEC0bits.T1IE = 1;    
+        IEC0bits.T1IE = 1;		//Interrupt Enabled    
         T1CONbits.TON=1;        
 }
 
-void Timer2_Init(void)
+void Timer2_Init(void)		//Generates Interrupt of 0.5 seconds (Timer used to control the Duty Cycle)
 {
-        T2CON = 0x0030;        
+        T2CON = 0x0030;		//Timer Prescale option of 1:256        
                                
         PR2 = 0xE100;  
         IFS0bits.T2IF = 0;      
-        IEC0bits.T2IE = 1;    
+        IEC0bits.T2IE = 1;		//Interrupt Enabled    
         T2CONbits.TON=1;        
 }
 
-void Timer3_Init(void)
+void Timer3_Init(void)		//Generates Interrupt of 0.1 seconds
 {
-        T3CON = 0x0030;        
+        T3CON = 0x0030;		//Timer Prescale option of 1:256        
                                
         PR3 = 0x2D00;  
         IFS0bits.T3IF = 0;      
-        IEC0bits.T3IE = 0;    
+        IEC0bits.T3IE = 0;		//Interrupt Disabled    
         T3CONbits.TON=1;        
 }
 
@@ -58,7 +58,7 @@ void __attribute__((__interrupt__)) _T1Interrupt(void)
 		{
 			loop = 0;	
 		}
-        IFS0bits.T1IF = 0; 
+        IFS0bits.T1IF = 0;		//Resets Timer1 Interrupt flag 
 		                     
 }
 
@@ -68,7 +68,7 @@ void __attribute__((__interrupt__)) _T2Interrupt(void)
 		ADC_flg = 1;
 		flag++;
 		
-		IFS0bits.T2IF = 0;                     
+		IFS0bits.T2IF = 0;		//Resets Timer2 Interrupt flag                     
 		
 }
 
@@ -76,5 +76,5 @@ void __attribute__((__interrupt__)) _T3Interrupt(void)
 {
              
        
-		IFS0bits.T3IF = 0;                     
+		IFS0bits.T3IF = 0;		//Resets Timer3 Interrupt flag                     
 }
